be1fbb4948f35463ea358147fab1f439e123e320
zh <missingone@zh.com>
2017-12-13 23:55:22 +0800

    登陆拦截器,页面转发拦截器  的基础实现

--- application/src/cn/singularity/appsys/intercept/LoginInterceptor.java
+++ application/src/cn/singularity/appsys/intercept/LoginInterceptor.java
@@ -1,25 +1,31 @@
 package cn.singularity.appsys.intercept;
 
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+/**
+ * 登陆拦截器
+ * 负责拦截后台管理员或者开发者操作时的登陆验证
+ * 
+ * @author zh
+ */
 public class LoginInterceptor extends HandlerInterceptorAdapter{
-
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		String username = (String)request.getSession().getAttribute("username");
 		String devUsername = (String)request.getSession().getAttribute("devUsername");
-		if (username == null) {
-			request.getRequestDispatcher("/WEB-INF/jsp/backendlogin.jsp").forward(request, response);
-			return false;
-		} else if (devUsername == null) {
-			request.getRequestDispatcher("/WEB-INF/jsp/devlogin.jsp").forward(request, response);
-			return false;
-		}
+		System.out.println("username" + username + " -- devUsername" + devUsername);
+//		if (username == null) {
+//			request.getRequestDispatcher("/WEB-INF/jsp/backendlogin.jsp").forward(request, response);
+//			return false;
+//		} else if (devUsername == null) {
+//			request.getRequestDispatcher("/WEB-INF/jsp/devlogin.jsp").forward(request, response);
+//			return false;
+//		}
 		return true;
 	}
 	
--- application/src/cn/singularity/appsys/intercept/PageInterceptor.java
+++ application/src/cn/singularity/appsys/intercept/PageInterceptor.java
@@ -13,23 +13,25 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  * 		包含/backend/则跳转到 backend目录
  * 在控制器中
  * handle方法返回jsp页面对应的逻辑视图名即可,无需添加目录
- * @author mk
+ * @author zh
  */
 public class PageInterceptor extends HandlerInterceptorAdapter{
 	
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		if (request.getRequestURI().indexOf("/dev/") >= 0) {
+		if (request.getRequestURI().indexOf("/statics/") >= 0) {
+			return;
+		} else if (request.getRequestURI().indexOf("/dev/") >= 0) {
 			modelAndView.setViewName("developer/" + modelAndView.getViewName());
-		} else if (request.getRequestURI().indexOf("/backend/") >= 0) {
+		} else if (request.getRequestURI().indexOf("/back/") >= 0) {
 			modelAndView.setViewName("backend/" + modelAndView.getViewName());
 		}
 		System.out.print("后拦截 :url = ");
-		System.out.print(request.getRequestURI());
+		System.out.println(request.getRequestURI());
 		System.out.print(":--dev = " + (request.getRequestURI().indexOf("/dev/") >= 0));
 		System.out.println(":--backend = " + (request.getRequestURI().indexOf("/backend/") >= 0));
-		System.out.println("viewname: " + modelAndView != null ? modelAndView.getViewName() : "");
+//		System.out.println("viewname: " + modelAndView != null ? modelAndView.getViewName() : "");
 	}
 
 
