a2b5202fca0685720f900ed56b518ce3e85f88e5
zh <missingone@zh.com>
2017-12-14 10:20:04 +0800

    修复添加重定向转发支持

--- application/src/cn/singularity/appsys/intercept/PageInterceptor.java
+++ application/src/cn/singularity/appsys/intercept/PageInterceptor.java
@@ -16,21 +16,38 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  * @author zh
  */
 public class PageInterceptor extends HandlerInterceptorAdapter{
-	
+	private final String bkPrefix = "developer/";
+	private final String devPrefix = "developer/";
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
+			
+		//如果是静态文件则不拦截
 		if (request.getRequestURI().indexOf("/statics/") >= 0) {
 			return;
-		} else if (request.getRequestURI().indexOf("/dev/") >= 0) {
-			modelAndView.setViewName("developer/" + modelAndView.getViewName());
-		} else if (request.getRequestURI().indexOf("/back/") >= 0) {
-			modelAndView.setViewName("backend/" + modelAndView.getViewName());
 		}
+		
+		if (modelAndView != null) {
+			String prefix = "";
+			if (modelAndView.getViewName().indexOf("redirect:") == 0){
+				prefix = "redirect: /dev/" + modelAndView.getViewName().substring(9).trim();
+			} else if(modelAndView.getViewName().indexOf("forward:") == 0) {
+				prefix = "forward: /back/" + modelAndView.getViewName().substring(8).trim();
+			} else  if (request.getRequestURI().indexOf("/dev/") >= 0) {
+				prefix = "backend/";
+			} else if (request.getRequestURI().indexOf("/back/") >= 0) {
+				prefix = "backend/";
+			} else {
+				return;
+			}
+			System.out.println("viewName=" + prefix);
+			modelAndView.setViewName(prefix);
+		}
+		
 		System.out.print("后拦截 :url = ");
 		System.out.println(request.getRequestURI());
 		System.out.print(":--dev = " + (request.getRequestURI().indexOf("/dev/") >= 0));
-		System.out.println(":--backend = " + (request.getRequestURI().indexOf("/backend/") >= 0));
+		System.out.println(":--back = " + (request.getRequestURI().indexOf("/back/") >= 0));
 //		System.out.println("viewname: " + modelAndView != null ? modelAndView.getViewName() : "");
 	}
 
