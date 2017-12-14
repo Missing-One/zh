fb6865f976ea4435c9e0644701d903bea6e40100
zh <missingone@zh.com>
2017-12-14 09:07:25 +0800

    添加事务支持

--- application/src/cn/singularity/appsys/service/impl/UserServiceImpl.java
+++ application/src/cn/singularity/appsys/service/impl/UserServiceImpl.java
@@ -1,6 +1,9 @@
 package cn.singularity.appsys.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import cn.singularity.appsys.mapper.UserMapper;
 import cn.singularity.appsys.pojo.User;
@@ -10,11 +13,14 @@ import cn.singularity.appsys.service.UserService;
  * 用户业务接口的一个实现类
  * @author zh
  */
+@Service
+@Transactional
 public class UserServiceImpl implements UserService{
 
 	@Autowired
 	private UserMapper userMapper;
 	
+	@Transactional(propagation = Propagation.SUPPORTS)
 	@Override
 	public User login(User user) {
 		return userMapper.selectOne(user);
