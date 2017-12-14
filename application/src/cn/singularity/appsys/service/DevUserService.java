41a9105de715958a3e021eab7d3b1dec8cf33aa9
zh <missingone@zh.com>
2017-12-14 15:54:46 +0800

    开发者登陆实现

---
+++ application/src/cn/singularity/appsys/service/DevUserService.java
@@ -0,0 +1,18 @@
+package cn.singularity.appsys.service;
+
+import cn.singularity.appsys.pojo.DevUser;
+
+/**
+ * 开发者服务接口
+ * @author zh
+ */
+public interface DevUserService {
+	
+	/**
+	 * 开发者登陆功能
+	 * @return
+	 * @author zh
+	 */
+	DevUser login(DevUser devUser);
+	
+}
