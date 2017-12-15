3b28b5d33335e21ee49f39cf2c9797d9008ee43f
zh <missingone@zh.com>
2017-12-15 14:16:16 +0800

    测试

---
+++ application/test/cn/singularity/appsys/mapper/CategoryMapperTest.java
@@ -0,0 +1,34 @@
+package cn.singularity.appsys.mapper;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.singularity.appsys.pojo.Category;
+/**
+ * 测试
+ * @author zh
+ */
+public class CategoryMapperTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		ApplicationContext app = new ClassPathXmlApplicationContext("app-dao.xml");
+		catMapper = app.getBean(CategoryMapper.class);
+	}
+	static CategoryMapper catMapper;
+
+	@Test
+	public void test() {
+		List<Category> list = catMapper.getCategoryListByParentId(null);
+		for (Category category : list) {
+			System.out.println(category.getCategoryname());
+		}
+	}
+
+}
